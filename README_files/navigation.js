7a82c5598d4749c1eaa711614753ab5fa1934d05
jean <jean@jean.com>
2015-05-21 13:40:35 +0800

    HW 1.1 Files

---
+++ README_files/navigation.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /courses/cse473/15sp/assignments/project1/js/navigation.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.29 (Fedora) Server at courses.cs.washington.edu Port 80</address>
+</body></html>
