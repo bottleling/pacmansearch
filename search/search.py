8402eaea4ffe53100cda33cbc85a6523d661bcf1
jean <jean@jean.com>
2015-05-28 15:19:52 +0800

    move node.py into search.py

--- search/search.py
+++ search/search.py
@@ -18,7 +18,6 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
-from node import Node
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
@@ -184,6 +183,38 @@ def aStarSearch(problem, heuristic=nullHeuristic):
                     frontier.heap.pop(h)
                     frontier.push(child, child.path_cost)
 
+class Node:
+    def __init__(self, state, parent=None, action=None, step_cost=1):
+        self.state = state
+        self.parent= parent #parent node
+        self.action=action #action on parent to get to this state
+        if (parent !=None):
+            self.path_cost = self.parent.path_cost + step_cost
+        else:
+            self.path_cost = step_cost
+
+    def __repr__(self):
+        s = "Node %s" % self.state
+        return s
+
+    def getChildren(self, problem): #from searchAgents
+        successors = problem.getSuccessors(self.state)
+        children = []
+        for s in successors:
+            node = Node(s[0],self,s[1],s[2])
+            children.append(node)
+        return children
+
+    def getPath(self):
+        current_node = self
+        path = []
+        while current_node:
+            if current_node.parent:
+                path.append(current_node.action)
+            current_node = current_node.parent
+        list.reverse(path)
+        #print path
+        return path
 
 
 # Abbreviations
